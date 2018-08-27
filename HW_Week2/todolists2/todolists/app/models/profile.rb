1aa8366db4a86727da9836f813836aee3b4cff5d
Zhuohui Li <L5622191423@gmail.com>
2018-08-26 23:46:11 -0700

    Homework for module 2

--- HW_Week2/todolists2/todolists/app/models/profile.rb
+++ HW_Week2/todolists2/todolists/app/models/profile.rb
@@ -1,3 +1,26 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :Detect_first_or_last_name
+  validate :Detect_gender
+  validate :Detect_conflict
+
+  def Detect_first_or_last_name
+  	if first_name.nil?  && last_name.nil?
+  		errors.add(:first_name, "First and last name are Null, You need to privode either a first or last name.")
+  	end
+  end
+
+  def Detect_gender
+  	if ((gender != 'male') or (gender != 'female'))
+  		errors.add(:gender, "You need to privode a correct gender.")
+  	end
+  end
+
+  def Detect_conflict
+   	if gender == 'male' && first_name == 'Sue'
+   		errors.add(:first_name, "Sue is a girl\'s name" )
+   	end
+  end
 end
+
--- HW_Week2/todolists2/todolists/db/seeds.rb
+++ HW_Week2/todolists2/todolists/db/seeds.rb
@@ -6,13 +6,60 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
 
-User.create! [
-	{username: "Fiorina", password_digest: "123456"},
-	{username: "Trump", password_digest: "123456"},
-	{username: "Carson", password_digest: "123456"},
-	{username: "Clinton", password_digest: "123456"},
+user1 = User.create!(username: "Fiorina", password_digest: "123456")
+user2 = User.create!(username: "Trump", password_digest: "123456")
+user3 = User.create!(username: "Carson", password_digest: "123456")
+user4 = User.create!(username: "Clinton", password_digest: "123456")
+
+user1.create_profile(gender: "female", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
+user2.create_profile(gender: "male", birth_year: 1946, first_name: "Donald", last_name: "Trump")
+user3.create_profile(gender: "male", birth_year: 1951, first_name: "Ben", last_name: "Carson")
+user4.create_profile(gender: "female", birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
+
+due_date = Date.today + 1.year
+user1.todo_lists << TodoList.create(list_name: "President Fiorina", list_due_date: due_date)
+user2.todo_lists << TodoList.create(list_name: "President Trump", list_due_date:  due_date)
+user3.todo_lists << TodoList.create(list_name: "President Carson", list_due_date:  due_date)
+user4.todo_lists << TodoList.create(list_name: "President Clinton", list_due_date:  due_date)
+
+item1 = [{due_date: due_date, title: "Florida votes", description: "Will win", completed: false},
+{due_date: due_date, title: "California votes", description: "Will lose", completed: true},
+{due_date: due_date, title: "Texas votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Pensylvania votes", description: "Will win", completed: false},
+{due_date: due_date, title: "New York votes", description: "Will lose", completed: true}]
+
+
+item2 =[
+{due_date: due_date, title: "Florida votes", description: "Will win", completed: false},
+{due_date: due_date, title: "California votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Texas votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Pensylvania votes", description: "Will win", completed: true},
+{due_date: due_date, title: "New York votes", description: "Will win", completed: true}
+]
+
+item3 = [
+{due_date: due_date, title: "Florida votes", description: "Will win", completed: false},
+{due_date: due_date, title: "California votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Texas votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Pensylvania votes", description: "Will win", completed: false},
+{due_date: due_date, title: "New York votes", description: "Will lose", completed: false}
+]
+
+item4 = [
+{due_date: due_date, title: "Florida votes", description: "Will win", completed: true},
+{due_date: due_date, title: "California votes", description: "Will win", completed: true},
+{due_date: due_date, title: "Texas votes", description: "Will lose", completed: true},
+{due_date: due_date, title: "Pensylvania votes", description: "Will win", completed: false},
+{due_date: due_date, title: "New York votes", description: "Will win", completed: true}
 ]
 
 
-User.find_by!(username: "Fiorina").profile.create(gender: "M", birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
+item1.each {|itemx| user1.todo_lists.first.todo_items << TodoItem.create!(itemx)}
+item2.each {|itemx| user2.todo_lists.first.todo_items << TodoItem.create!(itemx)}
+item3.each {|itemx| user3.todo_lists.first.todo_items << TodoItem.create!(itemx)}
+item4.each {|itemx| user4.todo_lists.first.todo_items << TodoItem.create!(itemx)}
+
