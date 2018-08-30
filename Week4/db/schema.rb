00bfeb42e0600a6e9385eec5c925b4903dc19a45
Zhuohui Li <L5622191423@gmail.com>
2018-08-29 23:43:55 -0700

    week4_note&HW

--- Week4/db/schema.rb
+++ Week4/db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20151015232430) do
+
   create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
@@ -38,4 +39,5 @@ ActiveRecord::Schema.define(version: 20151015232430) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
 end
